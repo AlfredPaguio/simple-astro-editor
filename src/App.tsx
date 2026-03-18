87ac583eead22ae77b11a22021380e4b5edbbd09
AlfredPaguio <alfredpaguio36@gmail.com>
2026-03-19 00:16:24 +0800

    Fix routing
    From https://stackoverflow.com/a/71985764

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import { TestConfigEditor } from "@/Tester";
 import MainEditor from "@/MainEditor";
-import { BrowserRouter, Routes, Route } from "react-router";
+import {
+  // BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router";
 import Providers from "@/components/Providers";
 
 export function App() {
   return (
     <Providers>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<MainEditor />} />
           <Route path="/tester" element={<TestConfigEditor />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Providers>
   );
 }
